2f92c35ff5381fee6f6e994c5d2442a31b2fad6e
ROCKFAL1 <sasha.spichak@mail.ru>
2022-02-25 16:53:47 +0300

    Added comments for 'storing mutext' ctors

--- include/sync/storing_mutex.hpp
+++ include/sync/storing_mutex.hpp
@@ -9,10 +9,34 @@ namespace sync {
     template<typename T>
     class storing_mutex final {
     public:
+
+        /**
+         * @brief Construct a new without data. If there is no content in this class,
+         * then when you try to get a value from this mutex, 
+         * you will get std::nullopt. In order to put an object, 
+         * you can use 'storing_mutex::reset()' and its overloads
+         */
         storing_mutex() = default;
+
+        /**
+         * @brief Construct a new storing mutex object
+         * 
+         * @param data will be copied into 'storing_mutex' object
+         */
         storing_mutex(T& data) : _data(data) {} 
+
+        /**
+         * @brief Construct a new storing mutex object
+         * 
+         * @param data will be moved into 'storing_mutex' object
+         */
         storing_mutex(T&& data) : _data(data) {} 
 
+        /**
+         * @brief Construct a new storing mutex object
+         * 
+         * @tparam Args is arguments for constructing an object of type 'T'
+         */
         template<typename ...Args>
         storing_mutex(Args&&... args) : _data(T(std::forward<Args>(args)...)) {}
 
